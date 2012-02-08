4c61c7eec9ea4d577d4e6c4f6385f36880f184a7
Daniel Hjort <daniel.hjort@gmail.com>
2012-02-08 21:25:38 +0700

    Added power exercise.

--- tutorial-1.lisp
+++ tutorial-1.lisp
@@ -18,6 +18,12 @@
       1
     (+ N (triangular (- N 1)))))
 
+(defun power (B E)
+  "Compute B to the power of E"
+  (if (= E 0)
+      1
+    (* B (power B (- E 1)))))
+
 ;; Tests
 
 (define-test my-double
@@ -38,4 +44,14 @@
   (assert-equal 3 (triangular 2))
   (assert-equal 6 (triangular 3))
   (assert-equal 10 (triangular 4))
+  )
+
+(define-test power
+  (assert-equal 0 (power 0 1))
+  (assert-equal 1 (power 1 0))
+  (assert-equal 0 (power 0 2))
+  (assert-equal 1 (power 2 0))
+  (assert-equal 2 (power 2 1))
+  (assert-equal 4 (power 2 2))
+  (assert-equal 8 (power 2 3))
   )
